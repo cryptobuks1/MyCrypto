be713f211e4f6fb66d50f323cc46b0335f390178
Frederik Bolding <frederik.bolding@gmail.com>
2020-08-25 12:56:26 +0200

    Fix some issues

--- src/features/TxStatus/helpers.ts
+++ src/features/TxStatus/helpers.ts
@@ -1,32 +1,33 @@
-import { ProviderHandler, getTxsFromAccount } from '@services';
+import { ProviderHandler, ITxHistoryEntry } from '@services';
 import {
   makeTxConfigFromTxResponse,
   makeTxConfigFromTxReceipt,
   makeUnknownTxReceipt
 } from '@utils';
-import { ITxType, ITxHash, NetworkId, StoreAccount, Asset, Network } from '@types';
+import { ITxType, ITxHash, NetworkId, StoreAccount, Asset, Network, ITxReceipt } from '@types';
 
 export const fetchTxStatus = async ({
   txHash,
   networkId,
   networks,
   accounts,
+  txCache,
   assets
 }: {
   txHash: string;
   networkId: NetworkId;
   networks: Network[];
   accounts: StoreAccount[];
+  txCache: ITxHistoryEntry[];
   assets: Asset[];
 }) => {
   const network = networks.find((n) => n.id === networkId)!;
-  const txCache = getTxsFromAccount(accounts);
   const cachedTx = txCache.find(
     (t) => t.hash === (txHash as ITxHash) && t.asset.networkId === networkId
   );
   if (cachedTx) {
     return {
-      config: makeTxConfigFromTxReceipt(cachedTx, assets, networks, accounts),
+      config: makeTxConfigFromTxReceipt(cachedTx as ITxReceipt, assets, networks, accounts),
       receipt: cachedTx
     };
   }
